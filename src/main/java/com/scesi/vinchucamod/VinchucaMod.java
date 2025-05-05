b233325d087d2d24d93124133d850995e62f1a9e
Steven Ramos <stevenjramossalazar@gmail.com>
2025-05-05 16:54:11 -0400

    Modificacion del item por defecto del molde, ahora cambiado a un tipo pocion y agregado de su nombre

--- src/main/java/com/scesi/vinchucamod/VinchucaMod.java
+++ src/main/java/com/scesi/vinchucamod/VinchucaMod.java
@@ -6,11 +6,11 @@ import com.mojang.logging.LogUtils;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.PotionItem;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -34,9 +34,7 @@ import net.minecraftforge.registries.RegistryObject;
 @Mod(VinchucaMod.MODID)
 public class VinchucaMod {
 
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "vinchucamod";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
@@ -57,16 +55,11 @@ public class VinchucaMod {
             () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties().setId(ITEMS.key("example_block")))
     );
 
-    // Creates a new food item with the id "examplemod:example_id", nutrition 1 and saturation 2
+    // Creates a new potion item with the id "examplemod:example_id"
     public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item",
-            () -> new Item(new Item.Properties()
+            () -> new PotionItem(new Item.Properties()
                     .setId(ITEMS.key("example_item"))
-                    .food(new FoodProperties.Builder()
-                            .alwaysEdible()
-                            .nutrition(1)
-                            .saturationModifier(2f)
-                            .build()
-                    )
+                    .stacksTo(1)
             )
     );
 
@@ -116,8 +109,8 @@ public class VinchucaMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
-            event.accept(EXAMPLE_BLOCK_ITEM);
+        if (event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS) { // Cambiar a la pestaña de Brewing
+            event.accept(EXAMPLE_ITEM);
         }
     }
 
