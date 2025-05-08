ef9f7754049c3a6797103b023d4f211f62aba5dd
Ricardo Ramirez <ricardoramirezcaero123@gmail.com>
2025-05-08 15:41:42 -0400

    feat: crea la clase MobEntities el cual registra la entidad VinchucaEntity

---
+++ src/main/java/com/scesi/vinchucamod/entity/MobEntities.java
@@ -0,0 +1,28 @@
+package com.scesi.vinchucamod.entity;
+
+import com.scesi.vinchucamod.VinchucaMod;
+import com.scesi.vinchucamod.entity.custom.VinchucaEntity;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+/**
+ * MobEntities
+ */
+public class MobEntities {
+    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES =
+        DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, VinchucaMod.MODID);
+    
+    public static final RegistryObject<EntityType<VinchucaEntity>> VINCHUCA = 
+        ENTITY_TYPES.register("vinchuca",
+            () -> EntityType.Builder.of(VinchucaEntity::new, MobCategory.MONSTER)
+            .sized(0.5f, 0.5f)
+            .build(ResourceKey.create(Registries.ENTITY_TYPE,
+            ResourceLocation.parse(VinchucaMod.MODID + ":vinchuca"))));
+}
